7edb9b0503ead84fb690ee3ff3cf6f2b14c7e78f
Reuben Berghan <reuben.berghan@gmail.com>
2017-01-26 13:44:14 +1100

    Init commit

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// init express
+const express = require('express');
+const app = express();
+
+// set PORT
+const PORT = process.env.PORT || 3000;
+app.set(`port`, PORT);
+
+// test res
+app.get(`/`, (req, res) => res.send(`Hello world...`));
+
+// start server
+app.listen(app.get(`port`), () => console.log(`Express server running on port ${ app.get(`port`) }`));
